3451a14e9246b8698dc83a90cb3130abdaf90afc
Phan Hoang An <anhoangphan96@gmail.com>
2023-06-17 11:23:37 +0700

    Updates

---
+++ precache-manifest.d8d449c8b36f8895937a84ab59827db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5bb30235a92d31976e202b259a0a7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d42be88114518024f8e4",
+    "url": "./static/css/main.05ea5fcc.chunk.css"
+  },
+  {
+    "revision": "a973b2fad990bda9f4bf",
+    "url": "./static/js/2.7b07825a.chunk.js"
+  },
+  {
+    "revision": "01c7198abf0be6996f8ecbde40e57f29",
+    "url": "./static/js/2.7b07825a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42be88114518024f8e4",
+    "url": "./static/js/main.12e063bf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.12e063bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc9fd83e633ebc6d38d",
+    "url": "./static/js/runtime-main.74b10c79.js"
+  }
+]);
